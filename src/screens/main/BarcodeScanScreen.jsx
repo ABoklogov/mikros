ebe07a1af254b8fb3801c891020bfde1c8f01263
Алексей Боклогов <kashira87@mail.ru>
2023-02-17 13:48:35 +0300

    начал изучение собственных модулей

--- src/screens/main/BarcodeScanScreen.jsx
+++ src/screens/main/BarcodeScanScreen.jsx
@@ -1,17 +1,10 @@
 import { useState, useEffect } from 'react';
 import { StyleSheet, Text } from 'react-native';
-import { CameraScreen } from 'react-native-camera-kit';
+
 
 export default function BarcodeScanScreen() {
   return (
-    <CameraScreen
-      // Barcode props
-      scanBarcode={true}
-      onReadCode={(event) => console.log(event)} // optional
-      showFrame={true} // (default false) optional, show frame with transparent layer (qr code or barcode will be read on this area ONLY), start animation for scanner,that stoped when find any code. Frame always at center of the screen
-      laserColor='red' // (default red) optional, color of laser in scanner frame
-      frameColor='white' // (default white) optional, color of border of scanner frame
-    />
+    <></>
   );
 }
 
