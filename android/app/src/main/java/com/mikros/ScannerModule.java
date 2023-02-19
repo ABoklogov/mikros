71aa68a253fb11b0e1ed9515c175255f33781ea5
Алексей Боклогов <kashira87@mail.ru>
2023-02-19 13:20:54 +0300

    Добавляет ScannerModule.java

---
+++ android/app/src/main/java/com/mikros/ScannerModule.java
@@ -0,0 +1,32 @@
+package com.mikros; // replace com.your-app-name with your app’s name
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import java.util.Map;
+import java.util.HashMap;
+import android.util.Log;
+
+public class ScannerModule extends ReactContextBaseJavaModule {
+  ScannerModule(ReactApplicationContext context) {
+    super(context);
+  }
+
+  @Override
+  public String getName() {
+    return "ScannerModule";
+  }
+  
+  @ReactMethod
+  public void fetchBarcode(String image, Promise promise) {
+    Log.d("ScannerModule", "Картинка штри-кода" + image);
+
+    try {
+      Integer code = '123456789'
+      promise.resolve(code);
+    } catch(Exception e) {
+      promise.reject("fetchBarcode error", e);
+    }
+  }
+}
