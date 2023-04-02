eebc95ed454c8c1816295e9ed819645c19b1e6f3
Алексей Боклогов <kashira87@mail.ru>
2023-04-02 21:45:40 +0300

    пытаюсь обновить сборщик

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context'; // чтобы не прыгала нижняя навигация при первой загрузке
 import { Provider as StoreProvider } from 'react-redux';
-// import { Provider as PaperProvider, MD3LightTheme as DefaultTheme } from 'react-native-paper';
 import { StatusBar, StyleSheet } from 'react-native';
 import SplashScreen from 'react-native-splash-screen'
 import { store } from 'store/index';
@@ -9,15 +8,6 @@ import { store } from 'store/index';
 import Main from 'components/Main';
 //import vars
 import { colors } from 'res/vars';
-
-// const theme = {
-//   ...DefaultTheme,
-//   colors: {
-//     ...DefaultTheme.colors,
-//     primary: 'tomato',
-//     secondary: 'yellow',
-//   },
-// };
 
 export default App = () => {
   // закрытие заставки
@@ -28,14 +18,12 @@ export default App = () => {
   return (
     <SafeAreaProvider>
       <StoreProvider store={store}>
-        {/* <PaperProvider theme={theme}> */}
         <StatusBar
           backgroundColor={colors.grey}
           barStyle="dark-content"
           hidden={false}
         />
         <Main />
-        {/* </PaperProvider> */}
       </StoreProvider>
     </SafeAreaProvider>
   );
